50f17bb2fe5928216047dc511f125acdda6730b0
Nikola Savic <snikolaa998@gmail.com>
2021-10-31 19:39:29 +0100

    commit

---
+++ app/src/main/java/com/example/musicplayer/exoplayer/MusicServiceConnection.kt
@@ -0,0 +1,93 @@
+package com.example.musicplayer.exoplayer
+
+import android.content.ComponentName
+import android.content.Context
+import android.media.browse.MediaBrowser
+import android.os.Bundle
+import android.support.v4.media.MediaBrowserCompat
+import android.support.v4.media.MediaMetadataCompat
+import android.support.v4.media.session.MediaControllerCompat
+import android.support.v4.media.session.PlaybackStateCompat
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.example.musicplayer.other.Constants.NETWORK_ERROR
+import com.example.musicplayer.other.Event
+import com.example.musicplayer.other.Resource
+
+class MusicServiceConnection(context: Context) {
+    //objekti koji sadrze informacije iz naseg servisa
+    private val _isConnected = MutableLiveData<Event<Resource<Boolean>>>()
+    val isConnected: LiveData<Event<Resource<Boolean>>> = _isConnected
+
+    private val _networkError = MutableLiveData<Event<Resource<Boolean>>>()
+    val networkError: LiveData<Event<Resource<Boolean>>> = _networkError
+
+    //Player state(pause, playing)
+    private val _playbackState = MutableLiveData<PlaybackStateCompat?>()
+    val playbackState: LiveData<PlaybackStateCompat?> = _playbackState
+
+    private val _curPlayingSong = MutableLiveData<MediaMetadataCompat?>()
+    val curPlayingSong: LiveData<MediaMetadataCompat?> = _curPlayingSong
+
+    //koristi se da se pusti pesma, pazuira..
+    lateinit var mediaController: MediaControllerCompat
+
+    private val mediaBrowserConnectionCallback = MediaBrowserConnectionCallback(context)
+
+    private val mediaBrowser = MediaBrowserCompat(context, ComponentName(context, MusicService::class.java), mediaBrowserConnectionCallback, null).apply {
+        connect()
+    }
+
+    val transportControls: MediaControllerCompat.TransportControls
+        get() = mediaController.transportControls
+
+    fun subscribe(parentId: String, callback: MediaBrowserCompat.SubscriptionCallback) {
+        mediaBrowser.subscribe(parentId, callback)
+    }
+
+    fun unsubscribe(parentId: String, callback: MediaBrowserCompat.SubscriptionCallback) {
+        mediaBrowser.unsubscribe(parentId, callback)
+    }
+
+    private inner class MediaBrowserConnectionCallback(private val context: Context) : MediaBrowserCompat.ConnectionCallback() {
+        override fun onConnected() {
+            mediaController = MediaControllerCompat(context, mediaBrowser.sessionToken).apply {
+                registerCallback(MediaControllerCallback())
+            }
+            _isConnected.postValue(Event(Resource.success(true)))
+        }
+
+        override fun onConnectionSuspended() {
+            _isConnected.postValue(Event(Resource.error("The connection was suspended", false)))
+        }
+
+        override fun onConnectionFailed() {
+            _isConnected.postValue(Event(Resource.error("Couldn't connect to media browser", false)))
+        }
+    }
+
+    private inner class MediaControllerCallback : MediaControllerCompat.Callback() {
+        override fun onPlaybackStateChanged(state: PlaybackStateCompat?) {
+            _playbackState.postValue(state)
+        }
+
+        override fun onMetadataChanged(metadata: MediaMetadataCompat?) {
+            _curPlayingSong.postValue(metadata)
+        }
+
+        override fun onSessionEvent(event: String?, extras: Bundle?) {
+            super.onSessionEvent(event, extras)
+            when(event) {
+                NETWORK_ERROR -> _networkError.postValue(
+                        Event(Resource.error("Please check your internet connection", null))
+                )
+            }
+        }
+
+        override fun onSessionDestroyed() {
+            super.onSessionDestroyed()
+            mediaBrowserConnectionCallback.onConnectionSuspended()
+        }
+    }
+
+}
